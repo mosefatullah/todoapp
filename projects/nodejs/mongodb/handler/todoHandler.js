eeb12a50d80e5fbbb99ebb5f62b5a0727e437956
mosefatullah <mosefatullah@gmail.com>
2024-01-15 16:51:43 +0600

    .

--- projects/nodejs/mongodb/handler/todoHandler.js
+++ projects/nodejs/mongodb/handler/todoHandler.js
@@ -9,7 +9,9 @@ const Todo = mongoose.model("Todo", todoSchema);
 // GET - get all todos
 router.get("/", async (req, res) => {
  try {
-  const data = await Todo.find();
+  const data = await Todo.find()
+   .limit(parseInt(req.body.endAt))
+   .skip(parseInt(req.body.startAt));
   res.status(200).json({
    message: "Todos were fetched successfully!",
    data,
@@ -23,17 +25,17 @@ router.get("/", async (req, res) => {
 
 // GET - get a todo by id
 router.get("/:id", async (req, res) => {
-    try {
-    const data = await Todo.findById(req.params.id);
-    res.status(200).json({
-    message: "Todo was fetched successfully!",
-    data,
-    });
-    } catch (err) {
-    res.status(500).json({
-    error: err.message || "Some error occurred while retrieving the Todo.",
-    });
-    }
+ try {
+  const data = await Todo.findById(req.params.id);
+  res.status(200).json({
+   message: "Todo was fetched successfully!",
+   data,
+  });
+ } catch (err) {
+  res.status(500).json({
+   error: err.message || "Some error occurred while retrieving the Todo.",
+  });
+ }
 });
 
 // POST - create a new todo
@@ -69,39 +71,42 @@ router.post("/bulk", async (req, res) => {
 
 // PUT - update a todo by id
 router.put("/:id", async (req, res) => {
-    try {
-        let updatedTodo = {
-            updatedAt: Date.now(),
-        };
-        if (req.body.title) updatedTodo.title = req.body.title;
-        if (req.body.description) updatedTodo.description = req.body.description;
-        if (req.body.status) updatedTodo.status = req.body.status;
-        await Todo.updateOne({_id: req.params.id}, {
-            $set: updatedTodo
-        });
-        res.status(200).json({
-            message: "Todo was updated successfully!",
-            data: updatedTodo,
-        });
-    } catch (err) {
-        res.status(500).json({
-            error: err.message || "Some error occurred while updating the Todo.",
-        });
-    }
+ try {
+  let updatedTodo = {
+   updatedAt: Date.now(),
+  };
+  if (req.body.title) updatedTodo.title = req.body.title;
+  if (req.body.description) updatedTodo.description = req.body.description;
+  if (req.body.status) updatedTodo.status = req.body.status;
+  await Todo.updateOne(
+   { _id: req.params.id },
+   {
+    $set: updatedTodo,
+   }
+  );
+  res.status(200).json({
+   message: "Todo was updated successfully!",
+   data: updatedTodo,
+  });
+ } catch (err) {
+  res.status(500).json({
+   error: err.message || "Some error occurred while updating the Todo.",
+  });
+ }
 });
 
 // DELETE - delete a todo by id
 router.delete("/:id", async (req, res) => {
-    try {
-        await Todo.deleteOne({_id: req.params.id});
-        res.status(200).json({
-            message: "Todo was deleted successfully!",
-        });
-    } catch (err) {
-        res.status(500).json({
-            error: err.message || "Some error occurred while deleting the Todo.",
-        });
-    }
+ try {
+  await Todo.deleteOne({ _id: req.params.id });
+  res.status(200).json({
+   message: "Todo was deleted successfully!",
+  });
+ } catch (err) {
+  res.status(500).json({
+   error: err.message || "Some error occurred while deleting the Todo.",
+  });
+ }
 });
 
 module.exports = router;
