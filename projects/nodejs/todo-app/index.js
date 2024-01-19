927a525a4c0e652d98c18553d9ee04d66f822c04
mosefatullah <mosefatullah@gmail.com>
2024-01-19 18:55:33 +0600

    .

--- projects/nodejs/todo-app/index.js
+++ projects/nodejs/todo-app/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 // database connection
 mongoose
- .connect("mongodb://localhost:27017/todo")
+ .connect(process.env.MONGODB_URI)
  .then(() => console.log("Connected to database"))
  .catch((err) => console.log(err));
 
