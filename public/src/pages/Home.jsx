4d43a1be0e80201050c31a6e8d5b43b017b6785a
mosefatullah <mosefatullah@gmail.com>
2024-01-20 20:31:51 +0600

    .

--- public/src/pages/Home.jsx
+++ public/src/pages/Home.jsx
@@ -232,8 +232,7 @@ function Home() {
    });
   } else {
    setAlert({
-    title:
-     "Mark as " + status.charAt(0).toUpperCase() + status.slice(1),
+    title: "Mark as " + status.charAt(0).toUpperCase() + status.slice(1),
     description:
      "Are you sure you want to mark the selected todo(s) as " + status + "?",
     open: true,
@@ -367,37 +366,13 @@ function Home() {
       </div>
      </div>
      <div className="todoTool max-w-3xl overflow-x-scroll">
-      <div className="flex space-x-2 justify-between items-center px-4 py-4 border-b bg-white border-gray-300 max-w-lg mx-auto whitespace-nowrap">
+      <div className="flex space-x-2 justify-between items-center px-4 py-4 border-b bg-white border-gray-300 max-w-xl mx-auto whitespace-nowrap">
        <div>
         <span className="text-gray-500 text-sm ml-auto">
          <span id="slectedNum">0</span> selected
         </span>
        </div>
        <div className="flex space-x-2">
-        <div>
-         <button
-          className="flex px-2 pr-3 py-1 text-sm bg-red-200 text-red-500 rounded-md font-medium hover:bg-red-300 transition duration-200 ease-in-out"
-          id="deleteSelectedTodos"
-          style={{ display: "none" }}
-          onClick={deleteSelectedTodos}
-         >
-          <svg
-           xmlns="http://www.w3.org/2000/svg"
-           fill="none"
-           viewBox="0 0 24 24"
-           strokeWidth={1.5}
-           stroke="currentColor"
-           className="w-5 h-5"
-          >
-           <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-           />
-          </svg>
-          <span className="ml-1">Delete</span>
-         </button>
-        </div>
         <div>
          <button
           className="flex px-2 pr-3 py-1 text-sm bg-green-200 text-green-500 rounded-md font-medium hover:bg-green-300 transition duration-200 ease-in-out"
@@ -426,14 +401,52 @@ function Home() {
         </div>
         <div>
          <button
-          className="flex px-2 pr-3 py-1 text-sm bg-yellow-200 text-yellow-500 rounded-md font-medium hover:bg-yellow-300 transition duration-200 ease-in-out"
+          className="flex px-2 pr-3 py-1 text-sm bg-purple-200 text-purple-500 rounded-md font-medium hover:bg-purple-300 transition duration-200 ease-in-out"
           id="markAsUndone"
           style={{ display: "none" }}
           onClick={() => {
            changeStatusOfTodo("undone");
           }}
          >
-          Undone
+          <svg
+           xmlns="http://www.w3.org/2000/svg"
+           fill="none"
+           viewBox="0 0 24 24"
+           strokeWidth={1.5}
+           stroke="currentColor"
+           className="w-5 h-5"
+          >
+           <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18 18 6M6 6l12 12"
+           />
+          </svg>
+          <span className="ml-1">Undone</span>
+         </button>
+        </div>
+        <div>
+         <button
+          className="flex px-2 pr-3 py-1 text-sm bg-red-200 text-red-500 rounded-md font-medium hover:bg-red-300 transition duration-200 ease-in-out"
+          id="deleteSelectedTodos"
+          style={{ display: "none" }}
+          onClick={deleteSelectedTodos}
+         >
+          <svg
+           xmlns="http://www.w3.org/2000/svg"
+           fill="none"
+           viewBox="0 0 24 24"
+           strokeWidth={1.5}
+           stroke="currentColor"
+           className="w-5 h-5"
+          >
+           <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+           />
+          </svg>
+          <span className="ml-1">Delete</span>
          </button>
         </div>
         <div>
@@ -486,7 +499,7 @@ function Home() {
      </div>
      <div className="flex justify-center max-w-3xl">
       <div
-       className="w-full mt-7 divide-y divide-gray-300 max-w-md space-y-4 mb-16"
+       className="w-full mt-7 divide-y divide-gray-300 max-w-lg space-y-4 mb-16"
        hidden={todos.length <= 0}
       >
        {todos.length > 0 &&
