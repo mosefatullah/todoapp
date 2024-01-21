e6b1e8fbf756e46c7502f4ca148a454afef3290e
mosefatullah <mosefatullah@gmail.com>
2024-01-21 18:05:49 +0600

    .

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,4 @@
-//const root = "https://gtodoapi.vercel.app";
-const root = "http://localhost:3000";
+const root = "https://gtodoapi.vercel.app";
 
 export const getTodos = async (error, callback) => {
  return await fetch(root + "/api/todo", {
